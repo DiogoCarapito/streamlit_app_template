2f9075ac5b5cb466cbba305f6570bfbc010d549c
DiogoCarapito <diogo.carapito@icloud.com>
2024-09-08 08:17:25 +0100

    test/test_app.py corrections

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,6 @@
 from streamlit.testing.v1 import AppTest
 
-pages = [
-    "app.py",
-    "pages/About.py",
-]
+pages = ["app.py", "pages/2_page.py", "pages/3_page.py"]
 
 for each_page in pages:
     test = AppTest(each_page, default_timeout=30)
