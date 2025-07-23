3e8c3fd91a4679a96d779e2baa6f97a70593a2a4
DiogoCarapito <diogo.carapito@icloud.com>
2025-07-23 08:04:12 +0100

    changes to accomodate streamlit 1.47.0

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ import streamlit as st
 
 
 def main():
-    st.title("Hello World!")
 
+    st.set_page_config(
+        page_title="My Streamlit App",
+        page_icon=":guardsman:",
+        layout="wide"
+    )
+
+    pg = st.navigation([
+        st.Page("pages/page_1.py"),
+        st.Page("pages/page_2.py")
+    ])
+
+    pg.run()
 
 if __name__ == "__main__":
     main()
